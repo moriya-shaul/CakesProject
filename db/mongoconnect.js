28d0806140afff650759b764ff6c9d74a52067f2
moriya <moriya0@gmail.com>
2023-06-01 12:39:02 +0300

    start

---
+++ db/mongoconnect.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const {config} = require("../config/secret")
+main().catch((err) => console.log(err));
+
+async function main() {
+  mongoose.set('strictQuery', false)
+  await mongoose.connect(
+    `mongodb+srv://${config.userDb}:${config.passDb}@cluster0.g50bscd.mongodb.net/db`
+    );
+    console.log("mongo connect")
+}
